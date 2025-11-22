cac2f511351e1cfa4dfc4fc01b151fd992331855
pharmarawasy-del <pharma.rawasy@gmail.com>
2025-11-22 11:20:13 +0200

    Mobile UX Polish

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="bg-gray-800 text-white py-6 mt-auto w-full">
+        <footer className="bg-gray-800 text-white py-6 mt-auto w-full pb-safe">
             <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-4" dir="rtl">
                 {/* Logo */}
                 <div className="text-xl font-bold">
